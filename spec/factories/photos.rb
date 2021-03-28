9e24a266138712a56e3b63c55c8d0a8945975650
篠塚　亮弥 <ryoya@shinodzarunoAir.lan>
2021-03-28 09:12:43 +0900

    フォトモデルをテスト

---
+++ spec/factories/photos.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :photo do
+    title "テスト"
+    body "内容"
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec/fixtures/sample.png"), 'image/png') }
+  end
+end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,4 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-end
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  it "写真が保存されること" do
+    photo = FactoryBot.build(:photo, user_id: @user.id)
+    photo.valid?
+    expect(photo).to be_valid
+  end
+  
+  it "タイトルがなければ無効であること" do
+    photo = Photo.new(title: nil)
+    photo.valid?
+    expect(photo.errors[:title]).to include("を入力してください")
+  end
+
+  it "内容がなければ無効であること" do
+    photo = Photo.new(body: nil)
+    photo.valid?
+    expect(photo.errors[:body]).to include("を入力してください")
+  end
+  
+  it "写真がなければ無効であること" do
+    photo = Photo.new(image: nil)
+    photo.valid?
+    expect(photo.errors[:image]).to include("を入力してください")
+  end
+
+  it "タイトルが20文字以上なら無効であること" do
+    photo = Photo.new(title: "a" * 21)
+    photo.valid?
+    expect(photo.errors[:title]).to include("は20文字以内で入力してください")
+  end
+
+  it "内容が150文字以上なら無効であること" do
+    photo = Photo.new(body: "a" * 151)
+    photo.valid?
+    expect(photo.errors[:body]).to include("は150文字以内で入力してください")
+  end
+ end
