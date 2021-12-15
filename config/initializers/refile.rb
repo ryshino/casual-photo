99fb1cb2d5196cc23e87c7dd1d89fc9e6c3d00ee
ryshino <ranature1153@gmail.com>
2021-12-15 16:02:35 +0900

    sass-railsを5から6に変更

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,7 +4,7 @@ aws = {
   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
   region: "ap-northeast-1",
-  bucket: "casual-ec-bucket",
+  bucket: "casual-photo",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
