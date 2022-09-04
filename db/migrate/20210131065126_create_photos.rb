d1cd1ef8b03eba3fc2b30eb1115e922863819b2a
ryshino <ranature1153@gmail.com>
2022-09-04 12:00:39 +0900

    マイグレーションファイルを変更

--- db/migrate/20210131065126_create_photos.rb
+++ db/migrate/20210131065126_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-      t.references :user_id, foreign_key: true
+      t.references :user, foreign_key: true
       t.string :title
       t.text :body
       t.string :image_id
